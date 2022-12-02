9d9fb8729c601678296b0afd97b2e318a97fbe06
Whizma <isak@maatta.se>
2022-12-02 13:45:12 +0100

    day2

--- day1.java
+++ day1.java
@@ -6,33 +6,8 @@ import java.util.Scanner;
 
 public class day1 {
     public static void main(String[] args) throws IOException {
-        Scanner scan;
-        int max = Integer.MIN_VALUE;
-        List<Integer> list = null;
-        try {
-            scan = new Scanner(new File("input/day1_1.txt"));
-            int current = 0;
-            list = new ArrayList<>();
-            while (scan.hasNextLine()) {
-                String temp = scan.nextLine();
-                if (temp.equals("")) {
-                    list.add(current);
-                    if (max < current) {
-                        max = current;
-                        current = 0;
-                    } else {
-                        current = 0;
-                    }
-                } else {
-                    current += Integer.parseInt(temp);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Collections.sort(list);
-        for (int i : list) {
-            System.out.println(i);
-        }
+        Files.lines(filePath)
+                .map(l -> l.split(" "))
+                .forEach(a -> createGraphe(a[0], a[1], a[2]));
     }
 }
