fed7eb4f34f5053f0bbb7164d541e70b47a5560a
Whizma <isak@maatta.se>
2022-12-08 15:07:10 +0100

    day7, kinda wonk

--- days/day7.java
+++ days/day7.java
@@ -9,6 +9,7 @@ public class day7 {
 
   private static Map<String, Integer> directorySizeMap = new HashMap<>();
   private static List<String> lines;
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new FileReader("input/day7.txt"));
     lines = new ArrayList<>();
@@ -22,62 +23,60 @@ public class day7 {
     String currentDir = "/";
     directorySizeMap.put(currentDir, 0);
     for (String input : lines) {
-        if (!isFirstFolder) {
-            if (input.startsWith("$ cd")) {
-                String dir = input.substring(5);
-                if ("..".equals(dir)) {
-                    currentDir = currentDir.substring(0, currentDir.lastIndexOf("/"));
-                } else {
-                    currentDir += "/" + dir;
-                    if (!directorySizeMap.containsKey(currentDir)) {
-                        directorySizeMap.put(currentDir, 0);
-                    }
-                }
-            } else if (Character.isDigit(input.charAt(0))) {
-                String[] commandResults = input.split(" ");
-                int fileSize = Integer.parseInt(commandResults[0]);
-                int currentDirSize = directorySizeMap.get(currentDir) + fileSize;
-                directorySizeMap.put(currentDir, currentDirSize);
-                updateParentFolderSizes(currentDir, fileSize);
+      if (!isFirstFolder) {
+        if (input.startsWith("$ cd")) {
+          String dir = input.substring(5);
+          if ("..".equals(dir)) {
+            currentDir = currentDir.substring(0, currentDir.lastIndexOf("/"));
+          } else {
+            currentDir += "/" + dir;
+            if (!directorySizeMap.containsKey(currentDir)) {
+              directorySizeMap.put(currentDir, 0);
             }
-        } else {
-            isFirstFolder = false;
+          }
+        } else if (Character.isDigit(input.charAt(0))) {
+          String[] commandResults = input.split(" ");
+          int fileSize = Integer.parseInt(commandResults[0]);
+          int currentDirSize = directorySizeMap.get(currentDir) + fileSize;
+          directorySizeMap.put(currentDir, currentDirSize);
+          updateParentFolderSizes(currentDir, fileSize);
         }
+      } else {
+        isFirstFolder = false;
+      }
     }
     System.out.println("Part one: " + partOne());
     System.out.println("Part two: " + partTwo());
   }
 
   private static void updateParentFolderSizes(String currentDirectory, int fileSize) {
-    while(!"/".equals(currentDirectory)) {
-        currentDirectory = currentDirectory.substring(0, currentDirectory.lastIndexOf("/"));
-        int currentDirSize = directorySizeMap.get(currentDirectory) + fileSize;
-        directorySizeMap.put(currentDirectory, currentDirSize);
+    while (!"/".equals(currentDirectory)) {
+      currentDirectory = currentDirectory.substring(0, currentDirectory.lastIndexOf("/"));
+      int currentDirSize = directorySizeMap.get(currentDirectory) + fileSize;
+      directorySizeMap.put(currentDirectory, currentDirSize);
     }
   }
 
   private static int partOne() {
     int largestSize = 100000;
     int sum = 0;
-    for (int dirSize: directorySizeMap.values()) {
-        if (dirSize <= largestSize) {
-            sum += dirSize;
-        }
+    for (int dirSize : directorySizeMap.values()) {
+      if (dirSize <= largestSize) {
+        sum += dirSize;
+      }
     }
     return sum;
   }
 
-private static int partTwo() {
+  private static int partTwo() {
     int unusedSpace = 70000000 - directorySizeMap.get("/");
     int sizeToFree = 30000000 - unusedSpace;
     int smallestFolderSize = 70000000;
-    for (int dirSize: directorySizeMap.values()) {
-        if (dirSize >= sizeToFree && dirSize < smallestFolderSize) {
-            smallestFolderSize = dirSize;
-        }
+    for (int dirSize : directorySizeMap.values()) {
+      if (dirSize >= sizeToFree && dirSize < smallestFolderSize) {
+        smallestFolderSize = dirSize;
+      }
     }
     return smallestFolderSize;
   }
-
 }
-
--- days/day8.java
+++ days/day8.java
@@ -3,15 +3,158 @@ package days;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class day8 {
-  public static void main(String[] args) throws FileNotFoundException {
+  public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new FileReader("input/day8.txt"));
-    dayOne(br);
-    dayTwo(br);
+    solve(br);
   }
 
-  private static void dayTwo(BufferedReader br) {}
+  private static void solve(BufferedReader br) throws IOException {
+    ArrayList<String> lines = new ArrayList<>();
+    String currentLine;
+    while ((currentLine = br.readLine()) != null) {
+      lines.add(currentLine);
+    }
+    int[][] trees = new int[99][99];
+    for (int row = 0; row < 99; row++) {
+      currentLine = lines.get(row);
+      char[] line = currentLine.toCharArray();
+      for (int col = 0; col < 99; col++) {
+        trees[row][col] = Character.getNumericValue(line[col]);
+      }
+    }
+    System.out.println("Part one: " + countVisibleTrees(trees));
+    System.out.println("Part two: " + getHighestScenicScore(trees));
+  }
 
-  private static void dayOne(BufferedReader br) {}
+  public static boolean checkWest(int[][] trees, int row, int col) {
+    int compareTree = trees[row][col];
+    for (int i = row - 1; i >= 0; i--) {
+      if (trees[i][col] >= compareTree) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean checkNorth(int[][] trees, int row, int col) {
+    int compareTree = trees[row][col];
+    for (int i = col - 1; i >= 0; i--) {
+      if (trees[row][i] >= compareTree) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean checkEast(int[][] trees, int row, int col) {
+    int compareTree = trees[row][col];
+    for (int i = row + 1; i < 99; i++) {
+      if (trees[i][col] >= compareTree) {
+        return false;
+      }
+    }
+    return true;  
+  }
+
+  public static boolean checkSouth(int[][] trees, int row, int col) {
+    int compareTree = trees[row][col];
+    for (int i = col + 1; i < 99; i++) {
+      if (trees[row][i] >= compareTree) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean checkTreeIsVisible(int[][] trees, int row, int col) {
+  return checkWest(trees, row, col) || 
+         checkEast(trees, row, col) || 
+         checkNorth(trees, row, col) || 
+         checkSouth(trees, row, col);
+  }
+
+  public static int countVisibleTrees(int[][] trees) {
+    int visibleTrees = 0;
+    for (int x = 0; x < 99; x++) {
+        for (int y = 0; y < 99; y++) {
+            if (checkTreeIsVisible(trees, x, y)) {
+                visibleTrees++;
+            }
+        }
+    }
+    return visibleTrees;
+  }
+
+  private static int getHighestScenicScore(int[][] trees) {
+        int highestScore = 0;
+        for (int row = 0; row < 99; row++) {
+            for (int col = 0; col < 99; col++) {
+                int scenicScore = getScenicScore(trees, row, col);
+                if (scenicScore > highestScore) {
+                    highestScore = scenicScore;
+                }
+            }
+        }
+        return highestScore;
+    }
+
+    private static int getScenicScore(int[][] trees, int row, int col) {
+        return getScenicWest(trees, row, col) * getScenicEast(trees, row, col) *
+                getScenicNorth(trees, row, col) * getScenicSouth(trees, row, col);
+    }
+
+    private static int getScenicWest(int[][] trees, int row, int col) {
+        int points = 0;
+        int compareTree = trees[row][col];
+        for (int i = row - 1; i >= 0; i--) {
+            points++;
+            if (trees[i][col] >= compareTree) {
+                break;
+            }
+        }
+        return points;
+    }
+
+    private static int getScenicEast(int[][] trees, int row, int col) {
+        int points = 0;
+        int compareTree = trees[row][col];
+        for (int i = row + 1; i < 99; i++) {
+            points++;
+            if (trees[i][col] >= compareTree) {
+                break;
+            }
+        }
+        return points;
+    }
+
+    private static int getScenicNorth(int[][] trees, int row, int col) {
+        int points = 0;
+        int compareTree = trees[row][col];
+        for (int i = col - 1; i >= 0; i--) {
+            points++;
+            if (trees[row][i] >= compareTree) {
+                break;
+            }
+        }
+        return points;
+    }
+
+    private static int getScenicSouth(int[][] trees, int row, int col) {
+        int points = 0;
+        int compareTree = trees[row][col];
+        for (int i = col + 1; i < 99; i++) {
+            points++;
+            if (trees[row][i] >= compareTree) {
+                break;
+            }
+        }
+        return points;
+    }
+
 }
