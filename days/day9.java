bf07e6c2b72e2a899986094626712af24fad8864
Whizma <isak@maatta.se>
2022-12-13 21:26:59 +0100

    day9 fuck p2

--- days/day9.java
+++ days/day9.java
@@ -3,199 +3,165 @@ package days;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class day9 {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new FileReader("input/day9test.txt"));
-    dayOne(br);
-    dayTwo(br);
-  }
 
-  private static void dayTwo(BufferedReader br) throws NumberFormatException, IOException {
-    String currentLine;
-    int xHead = 0;
-    int yHead = 0;
-    int xTail = 0;
-    int yTail = 0;
-    HashSet<List<Integer>> visited = new HashSet<>();
-    while ((currentLine = br.readLine()) != null) {
-      String[] input = currentLine.split(" ");
-      int distance = Integer.parseInt(input[1]);
-      String direction = input[0];
-      // If the head is ever two steps directly
-      // up, down, left, or right from the tail,
-      // the tail must also move one step in that direction
-      // so it remains close enough:
-      // Otherwise, if the head and tail aren't touching and
-      // aren't in the same row or column,
-      // the tail always moves one step diagonally to keep up
-      for (int i = 0; i < distance; i++) {
-        if (direction.equals("L")) {
-          xHead--;
-          // om samma rad, flytta tail vänster i x-led
-          if (yHead == yTail) {
-            if (separateTen(xHead, yHead, xTail, yTail)) {
-              xTail--;
-            }
-          }
-          // annars flytta vänster i x-led och ++ eller -- i y-led
-          else {
-            if (separateTen(xHead, yHead, xTail, yTail)) {
-              xTail--;
-              yTail = yHead;
-            }
-          }
-        }
-        if (direction.equals("U")) {
-          yHead++;
-          // om samma kolumn, flytta tail uppåt i y-led
-          if (xHead == xTail) {
-            if (separateTen(xHead, yHead, xTail, yTail)) {
-              yTail++;
-            }
-          }
-          // annars flytta uppåt i y-led och ++ eller -- i x-led
-          else {
-            if (separateTen(xHead, yHead, xTail, yTail)) {
-              yTail++;
-              xTail = xHead;
-            }
-          }
-        }
-        if (direction.equals("R")) {
-          xHead++;
-          // om samma rad, flytta tail höger i x-led
-          if (xHead == xTail) {
-            if (separateTen(xHead, yHead, xTail, yTail)) {
-              xTail++;
-            }
-          }
-          // annars flytta höger i x-led och ++ eller -- i y-led
-          else {
-            if (separateTen(xHead, yHead, xTail, yTail)) {
-              xTail++;
-              yTail = yHead;
-            }
-          }
-        }
-        if (direction.equals("D")) {
-          yHead--;
-          // om samma kolumn, flytta tail nedåt i y-led
-          if (xHead == xTail) {
-            if (separateTen(xHead, yHead, xTail, yTail)) {
-              yTail--;
-            }
-          }
-          // annars flytta nedåt i y-led och ++ eller -- i x-led
-          else {
-            if (separateTen(xHead, yHead, xTail, yTail)) {
-              yTail--;
-              xTail = xHead;
-            }
-          }
-        }
-        visited.add(List.of(xTail, yTail));
-      }
+    public static void main(String[] args) throws IOException {
+        System.out.println("Part one: " + partOne());
+        System.out.println("Part two: " + partTwo());
     }
-    System.out.println(visited.size());
-  }
 
-  private static void dayOne(BufferedReader br) throws IOException {
-    String currentLine;
-    int xHead = 0;
-    int yHead = 0;
-    int xTail = 0;
-    int yTail = 0;
-    HashSet<List<Integer>> visited = new HashSet<>();
-    while ((currentLine = br.readLine()) != null) {
-      String[] input = currentLine.split(" ");
-      int distance = Integer.parseInt(input[1]);
-      String direction = input[0];
-      // If the head is ever two steps directly
-      // up, down, left, or right from the tail,
-      // the tail must also move one step in that direction
-      // so it remains close enough:
-      // Otherwise, if the head and tail aren't touching and
-      // aren't in the same row or column,
-      // the tail always moves one step diagonally to keep up
-      for (int i = 0; i < distance; i++) {
-        if (direction.equals("L")) {
-          xHead--;
-          // om samma rad, flytta tail vänster i x-led
-          if (yHead == yTail) {
-            if (separate(xHead, yHead, xTail, yTail)) {
-              xTail--;
-            }
-          }
-          // annars flytta vänster i x-led och ++ eller -- i y-led
-          else {
-            if (separate(xHead, yHead, xTail, yTail)) {
-              xTail--;
-              yTail = yHead;
-            }
-          }
-        }
-        if (direction.equals("U")) {
-          yHead++;
-          // om samma kolumn, flytta tail uppåt i y-led
-          if (xHead == xTail) {
-            if (separate(xHead, yHead, xTail, yTail)) {
-              yTail++;
-            }
-          }
-          // annars flytta uppåt i y-led och ++ eller -- i x-led
-          else {
-            if (separate(xHead, yHead, xTail, yTail)) {
-              yTail++;
-              xTail = xHead;
-            }
-          }
-        }
-        if (direction.equals("R")) {
-          xHead++;
-          // om samma rad, flytta tail höger i x-led
-          if (xHead == xTail) {
-            if (separate(xHead, yHead, xTail, yTail)) {
-              xTail++;
-            }
-          }
-          // annars flytta höger i x-led och ++ eller -- i y-led
-          else {
-            if (separate(xHead, yHead, xTail, yTail)) {
-              xTail++;
-              yTail = yHead;
-            }
-          }
-        }
-        if (direction.equals("D")) {
-          yHead--;
-          // om samma kolumn, flytta tail nedåt i y-led
-          if (xHead == xTail) {
-            if (separate(xHead, yHead, xTail, yTail)) {
-              yTail--;
-            }
-          }
-          // annars flytta nedåt i y-led och ++ eller -- i x-led
-          else {
-            if (separate(xHead, yHead, xTail, yTail)) {
-              yTail--;
-              xTail = xHead;
-            }
-          }
-        }
-        visited.add(List.of(xTail, yTail));
-      }
+    public static int partOne() throws IOException {
+        List<Coordinate> rope = List.of(new Coordinate(0, 0), new Coordinate(0, 0));
+        return moveRope(rope);
     }
-    System.out.println(visited.size());
-  }
 
-  private static boolean separate(int xHead, int yHead, int xTail, int yTail) {
-    return Math.abs(xHead - xTail) > 1 || Math.abs(yHead - yTail) > 1;
-  }
+    public static int partTwo() throws IOException {
+        List<Coordinate> rope = List.of(new Coordinate(0, 0),
+                new Coordinate(0, 0), new Coordinate(0, 0), new Coordinate(0, 0),
+                new Coordinate(0, 0), new Coordinate(0, 0), new Coordinate(0, 0),
+                new Coordinate(0, 0), new Coordinate(0, 0), new Coordinate(0, 0));
+        return moveRope(rope);
+    }
 
-  private static boolean separateTen(int xHead, int yHead, int xTail, int yTail) {
-    return Math.abs(xHead - xTail) > 10 || Math.abs(yHead - yTail) > 10;
-  }
+    public static int moveRope(List<Coordinate> rope) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("input/day9.txt"));
+        Set<Coordinate> visitedLocations = new HashSet<>();
+        String currentLine;
+        while ((currentLine = br.readLine()) != null) {
+            String[] move = currentLine.split(" ");
+            String direction = move[0];
+            int times = Integer.parseInt(move[1]);
+
+            for (int i = 0; i < times; i++) {
+                if (direction.equals("R")) {
+                    moveRight(rope);
+                }
+
+                if (direction.equals("L")) {
+                    moveLeft(rope);
+                }
+
+                if (direction.equals("U")) {
+                    moveUp(rope);
+                }
+
+                if (direction.equals("D")) {
+                    moveDown(rope);
+                }
+                Coordinate tail = rope.get(rope.size() - 1);
+                visitedLocations.add(new Coordinate(tail.x, tail.y));
+            }
+
+
+        }
+        return visitedLocations.size();
+    }
+
+    public static void moveRight(List<Coordinate> rope) {
+        rope.get(0).x++;
+        for (int i = 1; i < rope.size(); i++) {
+            Coordinate head = rope.get(i - 1);
+            Coordinate tail = rope.get(i);
+            adjustTail(head, tail);
+        }
+    }
+
+    public static void moveLeft(List<Coordinate> rope) {
+        rope.get(0).x--;
+        for (int i = 1; i < rope.size(); i++) {
+            Coordinate head = rope.get(i - 1);
+            Coordinate tail = rope.get(i);
+            adjustTail(head, tail);
+        }
+    }
+
+    public static void moveUp(List<Coordinate> rope) {
+        rope.get(0).y++;
+        for (int i = 1; i < rope.size(); i++) {
+            Coordinate head = rope.get(i - 1);
+            Coordinate tail = rope.get(i);
+            adjustTail(head, tail);
+        }
+    }
+
+    public static void moveDown(List<Coordinate> rope) {
+        rope.get(0).y--;
+        for (int i = 1; i < rope.size(); i++) {
+            Coordinate head = rope.get(i - 1);
+            Coordinate tail = rope.get(i);
+            adjustTail(head, tail);
+        }
+    }
+    public static void adjustTail(Coordinate head, Coordinate tail) {
+        if (tail.x + 2 == head.x) {
+            if (tail.y < head.y) {
+                tail.y++;
+            } else if (tail.y > head.y) {
+                tail.y--;
+            }
+            tail.x = head.x - 1;
+        }
+
+        if (tail.x - 2 == head.x) {
+            if (tail.y < head.y) {
+                tail.y++;
+            } else if (tail.y > head.y) {
+                tail.y--;
+            }
+            tail.x = head.x + 1;
+        }
+
+        if (tail.y + 2 == head.y) {
+            if (tail.x < head.x) {
+                tail.x++;
+            } else if (tail.x > head.x) {
+                tail.x--;
+            }
+            tail.y = head.y - 1;
+        }
+
+        if (tail.y - 2 == head.y) {
+            if (tail.x < head.x) {
+                tail.x++;
+            } else if (tail.x > head.x) {
+                tail.x--;
+            }
+            tail.y = head.y + 1;
+        }
+    }
+
+    private static class Coordinate {
+        int x;
+        int y;
+
+        public Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Coordinate that = (Coordinate) o;
+            return x == that.x && y == that.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
 }
+
